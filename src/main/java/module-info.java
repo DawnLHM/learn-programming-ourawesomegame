d5d6e8cf6daba5272f4e1a585c81b1e032df7a89
Dawn McLaughlin <dawnm@andrew.cmu.edu>
2024-01-16 19:46:58 -0500

    Fixes are here!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module learn.programming.ourawesomegame {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.almasb.fxgl.all;
-
     opens learn.programming.ourawesomegame to javafx.fxml;
     exports learn.programming.ourawesomegame;
 }
