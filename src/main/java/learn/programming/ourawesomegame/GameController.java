8741fc57c89e1d466fb3e2d0c4608163fa29959e
Logan <loganpialagitis@gmail.com>
2024-01-31 19:51:53 -0500

    hi
    :x!

--- src/main/java/learn/programming/ourawesomegame/GameController.java
+++ src/main/java/learn/programming/ourawesomegame/GameController.java
@@ -1,14 +1,51 @@
 package learn.programming.ourawesomegame;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class GameController {
-    @FXML
-    private Label welcomeText;
+
+    @FXML private AnchorPane game_play;
+    @FXML private Canvas game_canvas;
+
+    @FXML private AnchorPane game_controls;
+
+    @FXML private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Sleepy!");
+    }
+    @FXML
+    private Button runningButton;
+
+    @FXML
+    protected void onHelloButtonClick2() {
+        runningButton.setLayoutX(30);
+        runningButton.setLayoutY(30);
+        boolean keepGoing=false;
+
+    }
+
+    @FXML
+    protected void runAwayFromTheMouse() {
+        runningButton.setStyle("-fx-background-color: green;");
+        float changeX = (float) (Math.random() * 300);
+        float changeY = (float) (Math.random() * 300);
+
+        TranslateTransition tt = new TranslateTransition(Duration.millis(500), runningButton);
+        tt.setRate(1);
+        tt.toXProperty().set(changeX);
+        tt.toYProperty().set(changeY);
+        tt.setCycleCount(1);
+
+        //Playing the animation
+        tt.play();
+
     }
 }
